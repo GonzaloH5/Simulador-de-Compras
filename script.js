a7621362fc0145384249e04e64cae1ab974ec7f9
Gonzalo <gonzalo20062010@gmail.com>
2026-02-02 18:49:14 -0300

    Se agrego descripcion sobre el proyecto en el archivo HTML

--- script.js
+++ script.js
@@ -3,10 +3,16 @@
 //---------------------------------------------
 
 const articulos = [
-    { id: 1, nombre: "Camisa", precio: 200 },
-    { id: 2, nombre: "Pantalones", precio: 400 },
-    { id: 3, nombre: "Zapatos", precio: 800 },
-    { id: 4, nombre: "Sombrero", precio: 300 }
+    { id: 1, nombre: "Ram 4gb DDR4", precio: 30 },
+    { id: 2, nombre: "Ram 8gb DDR4", precio: 60 },
+    { id: 3, nombre: "Ram 16gb DDR4", precio: 100 },
+    { id: 4, nombre: "GPU RTX 3060", precio: 350 },
+    { id: 5, nombre: "Fuente de poder 600W", precio: 70 },
+    { id: 6, nombre: "SSD 500gb", precio: 80 },
+    { id: 7, nombre: "SSD 1TB", precio: 150 },
+    { id: 8, nombre: "HDD 1TB", precio: 50 },
+    { id: 9, nombre: "HDD 2TB", precio: 80 },
+    { id: 10, nombre: "Placa madre B450", precio: 120 }
 ];
 
 let carrito = [];
@@ -36,18 +42,10 @@ function agregarAlCarrito(id) {
     }
 }
 
-function calcularTotal(carrito) {
-    let total = 0;
-    for (let articulo of carrito) {
-        total += articulo.precio;
-    }
-    return total;
-}
-
 while (seguircomprando) {
     let decision = prompt("¿Querés agregar un artículo al carrito? (si/no)").toLowerCase();
     if (decision === "si") {
-        alert(mostrarArticulos());
+        console.log(mostrarArticulos());
         let idElegido = prompt("Ingresá el numero de articulo que querés:");
         let id = Number(idElegido);
         let resultado = agregarAlCarrito(id);
@@ -57,5 +55,12 @@ while (seguircomprando) {
     }
 }
 
-alert(resultado);
+function calcularTotal(carrito) {
+    let total = 0;
+    for (let articulo of carrito) {
+        total += articulo.precio;
+    }
+    return total;
+}
+
 alert("Total a pagar: $" + calcularTotal(carrito));
